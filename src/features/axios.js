acea7adea2d6a19f11c76aa34f66a79e373e59d8
ArceDaniel <jonathandanielarce9@gmail.com>
2023-02-03 00:57:03 -0300

    deploy

--- src/features/axios.js
+++ src/features/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL || 'https://ph0-api-production.up.railway.app/'
+    baseURL: process.env.REACT_APP_BACKEND_URL || 'https://ph0-api-production.up.railway.app'
 });
 
 export default instance;

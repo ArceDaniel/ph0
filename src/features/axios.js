321982f84306f8b456fdd96afa72523b6196b6ed
ArceDaniel <jonathandanielarce9@gmail.com>
2023-02-03 00:55:23 -0300

    deploy

---
+++ src/features/axios.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+
+const instance = axios.create({
+    baseURL: process.env.REACT_APP_BACKEND_URL || 'https://ph0-api-production.up.railway.app/'
+});
+
+export default instance;
